07ba1be9c6a22b1ecd31bd14e3c76e592d918a19
karolWazny <karoljanwazny@gmail.com>
2021-03-28 17:52:46 +0100

    zapis do pliku #1 zapis wielu wierszy

--- write_message.py
+++ write_message.py
@@ -2,4 +2,5 @@ if __name__ == '__main__':
     filename = 'programming.txt'
 
     with open(filename, 'w') as file_object:
-        file_object.write("Uwielbiam programować.")
+        file_object.write("Uwielbiam programować.\n")
+        file_object.write("Uwielbiam tworzyć nowe gry.\n")
